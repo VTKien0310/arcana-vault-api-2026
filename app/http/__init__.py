097d1be8089302a9f72ef04566102de7251f95d8
trgkien.vu <trgkien.vu@gmail.com>
2026-02-21 10:28:14 +0700

    feat: Add collection router with list endpoint
    - Introduce `collection_router.py` to define routes related to collections.
    - Add `/collections` endpoint for listing user-specific collections with proper dependencies.
    - Update `__init__.py` to include `collection_router` in the router registry.

--- app/http/__init__.py
+++ app/http/__init__.py
@@ -1,4 +1,6 @@
 from app.http.key_router import router as key_router
 from app.http.item_router import router as item_router
+from app.http.collection_router import router as collection_router
 
-router_registry = [key_router, item_router]
+
+router_registry = [key_router, item_router, collection_router]
---
+++ app/http/collection_router.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+from app.data import Collection
+from app.features.authentication.guards import (
+    CurrentAuthenticatedUserDep,
+    AuthenticatedGuardDep,
+    SecretJwtGuardDep,
+)
+from app.features.item.services import ListCollectionsServiceDep
+
+router = APIRouter(
+    prefix="/collections",
+    tags=["collection"],
+    dependencies=[AuthenticatedGuardDep, SecretJwtGuardDep],
+)
+
+
+@router.get("/")
+async def list_collections(
+    current_user: CurrentAuthenticatedUserDep,
+    list_collections_service: ListCollectionsServiceDep,
+) -> list[Collection]:
+    collections = list_collections_service.handle(current_user)
+
+    return collections
