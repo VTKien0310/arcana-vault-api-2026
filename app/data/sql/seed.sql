cb7d547bd8d60c9cfa597761da6b356380273cda
trungkien <trgkien.vu@gmail.com>
2026-05-27 10:24:03 +0700

    feat: Initialize Alembic migrations with tables for collections and keys
    - Created initial Alembic revision `b019d2d86da0` to define schema for `collections` and `keys` in `arcana_vault`.
    - Added `collections` table with fields for `id`, `user_id`, `name`, and timestamps.
    - Added `keys` table with fields for `id`, `user_id`, `value`, `channels`, expiration, and optional contact details.
    - Updated `seed.sql` to adjust table references and include new fields with default timestamps.

--- app/data/sql/seed.sql
+++ app/data/sql/seed.sql
@@ -1,8 +1,12 @@
-INSERT INTO keys (user_id, value, channels, expiration, email, telegram_chat_id, phone)
-VALUES (gen_random_uuid(),
+INSERT INTO arcana_vault.keys (id, user_id, value, channels, expiration, email, telegram_chat_id, phone, created_at,
+                               updated_at)
+VALUES ('0709270f-8288-4ed3-89f3-63a92689b65f',
+        '7eecd02a-316f-42ac-8dae-48db22d2b6eb',
         '12345678',
         '{0, 1, 2}',
         now() + interval '30 days',
         'user@example.com',
         '123456789',
-        '+1234567890');
+        '+1234567890',
+        now(),
+        now());
